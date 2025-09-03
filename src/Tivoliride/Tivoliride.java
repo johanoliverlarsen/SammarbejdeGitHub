ddfe8568942058ccf77265a1edf2f8c80bc3c0d9
Cheriks <cher002@stud.ed.dk>
2025-09-03 12:10:43 +0200

    tilføjet en attribut

--- src/Tivoliride/Tivoliride.java
+++ src/Tivoliride/Tivoliride.java
@@ -5,7 +5,7 @@ import java.util.Scanner;
 private class Tivoliride {
     double hoejde =;
     double vægt;
-    double something = 40.0;
+    double something;
 
     public Tivoliride(int hoejde); {
         this.hoejde = hoejde
