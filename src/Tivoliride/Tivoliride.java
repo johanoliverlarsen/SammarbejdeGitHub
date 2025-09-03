eedca135a9a37077af5a11c06830ecc713cc3b48
johanoliverlarsen <johanoliverlarsen@gmail.com>
2025-09-03 11:03:14 +0200

    Tivoliride opgave

---
+++ src/Tivoliride/Tivoliride.java
@@ -0,0 +1,17 @@
+package Lesson04;
+
+import java.util.Scanner;
+
+private class Tivoliride {
+    double hoejde =;
+
+    public Tivoliride(hoejde) {
+        this.hoejde = hoejde
+    }
+
+    public Tivoliride(hoejde) {
+        Scanner scanner = new Scanner(System.in);
+    }
+
+
+}
